07077c7e6da766158496eb59a94706aa222ef035
VEDHASAI <pallintivedhasai@gmail.com>
2026-01-20 14:12:37 +0530

    config the image base path

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isProd ? '/Player-Auction-Application' : '',
+  assetPrefix: isProd ? '/Player-Auction-Application/' : '',
   images: {
     unoptimized: true,
   },
